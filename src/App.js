bc2e1bc701c88b5266f06502a9229813c744d8b5
sanjeevyg <yogi.sjv@gmail.com>
2022-04-09 22:49:19 -0400

    App.js cleaned

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ import { ThemeProvider } from './ThemeContext';
 function App() {
   
   return (
-    // <>
-    //   <ThemeContext.Provider value={darkTheme}>
-    //     <button onClick={toggleTheme}>Toggle Theme</button>
-    //     <FunctionContextComponent />
-    //   </ThemeContext.Provider>
-    // </>
     <ThemeProvider>
       <FunctionContextComponent />
     </ThemeProvider>
